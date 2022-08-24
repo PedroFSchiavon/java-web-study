8661fe18b1a89407cef4e29c5c149b2a6dba8da4
Pedro F Schiavon <pedrofschiavon@gmail.com>
2022-08-24 15:57:56 -0300

    Aprendendo sobre formularios e Post.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servelet/NovaEmpresaServlet.java
@@ -11,10 +11,18 @@ import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/novaempresa")
 public class NovaEmpresaServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+//    @Override
+//    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        String novaEmpresa = req.getParameter("nome");
+//        PrintWriter out = resp.getWriter();
+//        out.println("<html><body>A empresa " + novaEmpresa + " foi cadastrada com sucesso!</body></html>");
+//        System.out.println("Nova empresa cadastrada com sucesso");
+//    }
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String novaEmpresa = req.getParameter("nome");
-        PrintWriter out = resp.getWriter();
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String novaEmpresa = request.getParameter("nome");
+        PrintWriter out = response.getWriter();
         out.println("<html><body>A empresa " + novaEmpresa + " foi cadastrada com sucesso!</body></html>");
         System.out.println("Nova empresa cadastrada com sucesso");
     }
