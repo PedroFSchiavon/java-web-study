d5d8936017cb21bd3b16d06a93ff621c979ad723
Pedro F Schiavon <pedrofschiavon@gmail.com>
2022-08-30 17:49:23 -0300

    Atualizando nome das classes.

--- gerenciador/src/main/java/br/com/alura/gerenciador/servelet/ListaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servelet/ListaEmpresaServlet.java
@@ -8,12 +8,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(urlPatterns = "/listaempresa")
-public class ListaEmpresa extends HttpServlet {
+public class ListaEmpresaServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
