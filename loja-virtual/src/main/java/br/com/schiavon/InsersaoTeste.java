eeff5047a7a6420a59195350705496b122cfa687
Pedro F Schiavon <pedrofschiavon@gmail.com>
2022-09-21 15:23:24 -0300

    Adiciando transações em meus inserts.

--- loja-virtual/src/main/java/br/com/schiavon/InsersaoTeste.java
+++ loja-virtual/src/main/java/br/com/schiavon/InsersaoTeste.java
@@ -5,32 +5,41 @@ import java.sql.*;
 public class InsersaoTeste {
     public static void main(String[] args) throws SQLException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        try (Connection connection = connectionFactory.criaConexao()) {
+
+        try(Connection connection = connectionFactory.criaConexao()){
             connection.setAutoCommit(false);
-            PreparedStatement statement = connection
+            try (PreparedStatement statement = connection
                     .prepareStatement("insert into PRODUTO (nome, descricao) values (?, ?)",
-                            Statement.RETURN_GENERATED_KEYS);
-            insereDados("Gelo", "Algo bem gelado", statement);
-            insereDados("Cha", "Algo bem quente", statement);
+                            Statement.RETURN_GENERATED_KEYS);) {
+                insereDados("Graveto", "Algo bem comum, por que alguem compraria?", statement);
+                insereDados("Diamante", "Algo bem caro para alguem comprar!", statement);
 
-            connection.commit();
-        } catch (SQLException e) {
+                connection.commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                connection.rollback();
+                System.out.println("ROLLBACK EXECUTADO.");
+            } finally {
+                System.out.println("Fim do update.");
+            }
+        }catch (Exception e){
             e.printStackTrace();
-            System.out.println("ROLLBACK EXECUTADO.");
-
-        } finally {
-            System.out.println("Fim do update.");
         }
+
+
     }
 
     private static void insereDados(String nome, String descricao, PreparedStatement statement) throws SQLException {
         statement.setString(1, nome);
         statement.setString(2, descricao);
         statement.executeUpdate();
-        ResultSet generatedKeys = statement.getGeneratedKeys();
-        while (generatedKeys.next()) {
-            int id = generatedKeys.getInt(1);
-            System.out.println("Gerei o produto de ID: " + id);
+        try(ResultSet generatedKeys = statement.getGeneratedKeys()) {
+            while (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                System.out.println("Gerei o produto de ID: " + id);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
