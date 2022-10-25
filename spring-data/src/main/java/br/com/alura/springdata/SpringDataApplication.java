6e4a834046af778017f5b42b1791b08eaa88e42b
Pedro F Schiavon <pedrofschiavon@gmail.com>
2022-10-25 17:56:12 -0300

    Criando repository e refatorando pacotes.

--- spring-data/src/main/java/br/com/alura/springdata/SpringDataApplication.java
+++ spring-data/src/main/java/br/com/alura/springdata/SpringDataApplication.java
@@ -1,13 +1,28 @@
 package br.com.alura.springdata;
 
+import br.com.alura.springdata.model.Cargo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.alura.springdata.repository.CargoRepository;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+    private final CargoRepository repository;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataApplication.class, args);
-	}
+    public SpringDataApplication(CargoRepository repository){
+        this.repository = repository;
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Cargo cargo = new Cargo();
+        cargo.setDescricao("DESENVOLVEDOR DE SOFTWARE");
+
+        repository.save(cargo);
+    }
 }
--- spring-data/src/main/java/br/com/alura/springdata/model/Cargo.java
+++ spring-data/src/main/java/br/com/alura/springdata/model/Cargo.java
@@ -1,4 +1,4 @@
-package model;
+package br.com.alura.springdata.model;
 
 import javax.persistence.*;
 
